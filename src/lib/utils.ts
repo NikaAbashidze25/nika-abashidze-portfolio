12f94af5d3bde8b4da3faef849f678d3d00464ab
Nika Abashidze <n.abashidze25@gmail.com>
2025-10-15 20:34:12 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-ls public/video/import { clsx, type ClassValue } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
